b22cb2921a12e67999666dd35c84ce35e55ee12f
Victor Renan Covalski Junes <tech.owned@gmail.com>
2022-05-23 17:29:31 -0300

    feat: report now includes iam user access key info and also if the user credentials are stored in a secrets manager instance within the same region as the lambda function

--- lambda/consolidate_report.py
+++ lambda/consolidate_report.py
@@ -2,27 +2,15 @@ import boto3
 import os
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
 
 BUCKET_NAME = os.environ['BUCKET_NAME']
-MAX_WORKERS = os.environ.get('MAX_WORKERS', '4')
-BATCH_SIZE = os.environ.get('BATCH_SIZE', '125')
+MAX_WORKERS = int(os.environ.get('MAX_WORKERS', '4'))
+BATCH_SIZE = int(os.environ.get('BATCH_SIZE', '125'))
 
-HEADER=(
-    'user,arn,user_creation_time,password_enabled,'
-    'password_last_used,password_last_changed,'
-    'password_next_rotation,mfa_active,'
-    'access_key_1_active,access_key_1_last_rotated,'
-    'access_key_1_last_used_date,access_key_1_last_used_region,'
-    'access_key_1_last_used_service,access_key_2_active,'
-    'access_key_2_last_rotated,access_key_2_last_used_date,'
-    'access_key_2_last_used_region,access_key_2_last_used_service,'
-    'cert_1_active,cert_1_last_rotated,cert_2_active,cert_2_last_rotated,'
-    'account_id'
-    )
-
-s3 = boto3.client('s3')
 
 def list_objects():
+    s3 = boto3.client('s3')
     now = datetime.now().strftime(r"%Y/%m/%d")
     prefix = f'{now}/'
     paginator = s3.get_paginator('list_objects_v2')
@@ -31,69 +19,65 @@ def list_objects():
             keys = page['Contents']
         except:
             break
-        
         for c in keys:
             yield c['Key']
 
-def adjust_report(body, key):
-    fname = key[key.rfind('/')+1:]
-    account_id = fname[fname.find('-')+1:fname.find('.')]
-
-    lines = body.splitlines(keepends=False)
-    rows = lines[1:]
-
-    for i in range(len(rows)):
-        rows[i] += f',{account_id}'
-    
-    entries = '\n'.join(rows)
-
-    return entries
-
 def download_reports_parallel(bucket, keys, workers=4, chunksize=25):
-    def get_reports(keys):
-        entries = []
-        for k in keys:
-            r = s3.get_object(Bucket=bucket, Key=k)
-            body = r['Body'].read().decode('utf-8')
-            entries.append(adjust_report(body, k))
-        return entries
-
     def chunks(lst, n):
         for i in range(0, len(lst), n):
             yield lst[i:i+n]
-    
+
+
     with ThreadPoolExecutor(max_workers=workers) as tp:
-        for reports in tp.map(get_reports, chunks(keys, chunksize)):
+        for reports in tp.map(download_reports, repeat(bucket), chunks(keys, chunksize)):
             for r in reports:
                 yield r
 
-def download_reports(bucket, keys, **kwargs):
-    
-    for k in keys:
-        r = s3.get_object(Bucket=bucket, Key=k)
-        body = r['Body'].read().decode('utf-8')
-        entries = adjust_report(body, k)
-        yield entries
+def get_object(s3_client, bucket: str, key: str):
+    r = s3_client.get_object(Bucket=bucket, Key=key)
+    return r['Body'].read().decode('utf-8')
 
-def consolidate_report():
+def download_reports(bucket, keys, **kwargs):
+    s3 = boto3.client('s3')
+    for k in keys:
+        body = get_object(s3, bucket, k)
+        yield body
+
+def get_csv_header(report: str):
+  return report[:report.find('\n')]
+
+def get_csv_rows(report: str, headerlen: int = None):
+  if headerlen:
+    return report[headerlen+1:]
+  return report[report.find('\n')+1:]
+
+def download_and_consolidate_report(workers: int = 1, batch_size: int = 25):
     
     keys = list(list_objects())
+    
     if int(MAX_WORKERS) > 1:
-        entries = [e for e in download_reports_parallel(BUCKET_NAME, keys, int(MAX_WORKERS), int(BATCH_SIZE))]
+        reports = [e for e in download_reports_parallel(BUCKET_NAME, keys, workers, batch_size)]
     else:
-        entries = [e for e in download_reports(BUCKET_NAME, keys)]
+        reports = [e for e in download_reports(BUCKET_NAME, keys)]
     
-    if len(entries) == 0:
+    if len(reports) == 0:
         return
+
+    header = get_csv_header(reports[0])    
+
+    # Join reports
+    body = ''.join(get_csv_rows(r, len(header)) for r in reports)
+
+    consolidated_report = (header + '\n' + body).encode('utf-8')
     
-    entries.insert(0, HEADER)
-    report = '\n'.join(entries)
     date = datetime.now().strftime(r"%Y-%m-%d")
-    key = f'consolidados/report-{date}.csv'
-    s3.put_object(Bucket=BUCKET_NAME, Key=key, Body=report.encode('utf-8'))
+    key = f'consolidated/report-{date}.csv'
+    
+    s3 = boto3.client('s3')
+    s3.put_object(Bucket=BUCKET_NAME, Key=key, Body=consolidated_report)
 
 def handler(event, context):
-    consolidate_report()
+    download_and_consolidate_report(workers=MAX_WORKERS)
 
 if __name__ == '__main__':
     handler(None, None)
